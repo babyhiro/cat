e1dd832b019fb1bca336883797937a7394c0b577
Your Name <benjanduhiu@gmail.com>
2018-02-22 10:50:56 +0300

    cat

---
+++ aboutus.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body style="background-image: url(kali.jpg); background-size: 100%; background-repeat: no-repeat;">
+
+		
+			<nav class="navbar navbar-default">
+		  		<div class="container-fluid">
+		    		<div class="navbar-header">
+		      			<a class="navbar-brand" href="aboutus.php"><span>EDDY HIGH TESTS</span></a>
+		    		</div>
+		    			<ul class="nav navbar-nav">
+		      				<li class="active"><a href="cat.php">Home</a></li>
+		      				<li><a href="gradecal.php">Get Results</a></li>
+		    			</ul>
+		        <ul class="nav navbar-nav navbar-right">
+		       		<li><a href="register.php"><span class="glyphicon glyphicon-user"></span>SignUp</a></li>
+		      		<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+		    	</ul>
+		    </div>
+		</nav>
+		<div style="background-color: #FFFFFF">
+			<h1 style="text-align: center;"><u>EDDY HIGH TESTS</h1></u>
+
+			<p style="text-align: center;">We present actionable, research-based parenting information, including articles, videos, podcasts, and interactive tips, to help parents engage with and support their children’s academic and social-emotional development at every grade and stage from preK through 12th grade. 
+			We provide parents essential information to help improve educational opportunities for their children
+		     <br>
+		 Eddy High is a co-educational day and boarding school for students aged 2-19, offering an adapted form of the British National Curriculum to Kenyan and international students. The school strives to develop each student's intellectual, emotional, physical and social growth to their full potential.
+
+At Eddy High we believe that by adopting clearly defined expectations, we will provide the students with the security necessary to achieve their potential within a relaxed but academically rigorous environment.
+
+As a Round Square School Eddy High believes in:<br>
+-Clear expectations in relation to academic performance and standards of behaviour
+-Providing the opportunity for our students to learn the skills necessary to further their education and to function successfully in the modern world
+-Positive reinforcement which will provide students with the confidence and self-esteem necessary to succeed
+-Stimulating intellectual curiosity and a desire to learn
+
+-Instilling in students an awareness of the environment and the need to preserve it
+-Fostering within each child a desire to take responsibility for self-motivation, self-direction, self-discipline and a positive self-image.</p></div>
+			<br>
+			<br>
+
+
+
+	
+
+</body>
+</html>
---
+++ cat.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tests</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body style="background-image: url(edd.jpeg); background-size: 90%; background-repeat: no-repeat;">
+		
+				<nav class="navbar navbar-default">
+		  		<div class="container-fluid">
+		    		<div class="navbar-header">
+		      			<a class="navbar-brand" href="aboutus.php"><span>EDDY HIGH TESTS</span></a>
+		    		</div>
+		    			<ul class="nav navbar-nav">
+		      				<li class="active"><a href="cat.php">Home</a></li>
+		      				<li><a href="gradecal.php">Get Results</a></li>
+		    			</ul>
+		        <ul class="nav navbar-nav navbar-right">
+		       		<li><a href="register.php"><span class="glyphicon glyphicon-user"></span> SignUp</a></li>
+		      		<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+		    	</ul>
+		  </div>
+		</nav>
+	<h1 style="text-align: center;">EDDY HIGH TESTS</h1>
+	-<div class="container">
+  <h2>REACH FOR THE STARS</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="dope.jpg" alt="School Is FUN" style="width:50%;">
+      </div>
+
+      <div class="item">
+        <img src="stress.jpeg" alt="Take You Through Everything" style="width:50%; height: 50%">
+      </div>
+    
+      <div class="item">
+        <img src="nurse.jpeg" alt="We Care" style="width:50%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<p style="text-align: center;">
+
+Our new GreatSchools Summary Rating offers expanded information about important aspects of school quality — including student access to advanced courses, student progress information, and equity data to help parents see whether or not all students are being served similarly — that parents across the country can use to support their children’s education and take action to improve their local schools. We’re incorporating this rich data to ensure that our school ratings reflect factors that we know are important to parents and that research shows are connected to students’ success in college and life. </p>
+
+
+
+</body>
+</html>
